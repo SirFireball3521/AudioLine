f4528ee68c3ce95e7bec99d946a4d7ce7f104fdf
SirFireball <sirfireball@shellshock>
2021-02-12 14:33:55 -0700

    removed node_mp3_player, I found a simpler library

--- index.js
+++ index.js
@@ -3,4 +3,23 @@ const blessed = require('blessed');
 const ytdl = require('ytdl-core');
 const FfmpegCommand = require('fluent-ffmpeg');
 const { createAudio } = require('node-mp3-player');
+const Audio = createAudio();
 
+var screen = blessed.screen({
+   smartCSR: true
+});
+
+//input
+screen.key(['p'], (ch, key) => {
+   testSong.stop();
+});
+screen.key(['escape'], () =>{
+   return process.exit(0);
+});
+
+// (async () => {
+//    const testSong = await Audio('songs/unravel.mp3');
+//    await testSong.volume(0.5);
+//    await testSong.play();
+// })();
+const testSong = Audio('songs/unravel.mp3');
