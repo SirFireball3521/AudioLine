a779aa1fa7be105f1487cb043347d5e4eb62f1a3
SirFireball <sirfireball@shellshock>
2021-02-12 13:46:42 -0700

    Added all the files.

---
+++ index.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const blessed = require('blessed');
+const ytdl = require('ytdl-core');
+const FfmpegCommand = require('fluent-ffmpeg');
+const { createAudio } = require('node-mp3-player');
+
